c90ac19adde5afd298ea92730450625690cb45f0
yjhoo98 <yjhoo98@gmail.com>
2025-02-24 16:30:13 +0900

    v0.7 custom module

--- day11.py
+++ day11.py
@@ -1,19 +1,18 @@
-# from statistics import LinearRegression
-from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsRegressor
+# from sklearn.linear_model import LinearRegression
+from yjhlearn import LinearRegression
 import pandas as pd
 import matplotlib.pyplot as plt
 ls =pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
-# print(ls)
+
 X=ls[["GDP per capita (USD)"]].values
 y=ls[["Life satisfaction"]].values
-# print(y)
-# ls.plot(kind='scatter',grid=True,
-#         x="GDP per capita (USD)",y="Life satisfaction")
-# plt.axis([23_500,62_500,4,9])
-# plt.show()
-# model=LinearRegression()
-model=KNeighborsRegressor(n_neighbors=3)
+
+ls.plot(kind='scatter',grid=True,
+        x="GDP per capita (USD)",y="Life satisfaction")
+plt.axis([23_500,62_500,4,9])
+plt.show()
+model=LinearRegression()
+
 model.fit(X,y)
 
 X_new=[[37655.2]]
---
+++ yjhlearn.py
@@ -0,0 +1,27 @@
+import numpy as np
+class LinearRegression:
+    def __init__(self):
+        self.slope=None #기울기
+        self.intercept=None #y절편
+    def fit(self,X,y):
+        """
+        learning function
+        :param X: independent variable(2d array format)
+        :param y: dependent variable(2d array format)
+        :return: void
+        """
+        X_mean=np.mean(X)
+        y_mean=np.mean(y)
+
+        denominator=np.sum(pow(X-X_mean,2))
+        numerator=np.sum((X-X_mean)*(y-y_mean))
+        self.slope=numerator/denominator
+        self.intercept=y_mean-(self.slope*X_mean)
+    def predict(self,X)->np.ndarray:
+            """
+            predict value for input
+            :param self:
+            :param X: new independent variable
+            :return: predict value for input(2d array format)
+            """
+            return self.slope*np.array(X)+self.intercept
