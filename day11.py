3b91a80848b56066d717ad64d345790f83c82c90
yjhoo98 <yjhoo98@gmail.com>
2025-02-24 11:37:56 +0900

    v0.4 pandas dataframe2

--- day11.py
+++ day11.py
@@ -2,9 +2,12 @@ from traceback import print_tb
 
 import numpy as np
 import pandas as pd
-df=pd.DataFrame({
-    "a":[4,5,6],
-    "b":[7,8,9],
-    "c":[10,11,12]},index=[1,2,3]
+df=pd.DataFrame(
+    [
+        [4,7,10],
+        [5,8,11],
+        [6,9,12]],
+    index=[1,2,3],
+    columns=['a','b','c']
 )
 print(df)
