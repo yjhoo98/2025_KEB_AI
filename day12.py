bddc7efeb264d92a8803d1eadc89a9ad8f4661de
yjhoo98 <yjhoo98@gmail.com>
2025-02-25 16:44:18 +0900

    1.1 day12 assignment2

--- day12.py
+++ day12.py
@@ -1,9 +1,9 @@
 #assignment
-#v0.9 day12 파일의 결측치 값->산술평균으로 채우기
+#v1.0 day12 파일의 결측치 값->산술평균으로 채우기(without sklearn)
 
 import numpy as np
 import pandas as pd
-from sklearn.impute import SimpleImputer
+# from sklearn.impute import SimpleImputer
 
 df=pd.DataFrame(
     {
@@ -12,6 +12,11 @@ df=pd.DataFrame(
         'C':[1,2,3,4]
     }
 )
-i=SimpleImputer(strategy='mean')
-df[['A','B']]=i.fit_transform(df[['A','B']])
+# i=SimpleImputer(strategy='mean')
+# df[['A','B']]=i.fit_transform(df[['A','B']])
+
+
+df.fillna({'A': df['A'].mean()}, inplace=True)
+
+df.fillna({'B': df['B'].mean()}, inplace=True)
 print(df)
